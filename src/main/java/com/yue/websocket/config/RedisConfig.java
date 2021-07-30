e1cc29799f693251b88b491801143f7ee3144eda
YUE <610696148@qq.com>
2021-07-30 17:46:06 +0800

    redis

--- src/main/java/com/yue/websocket/config/RedisConfig.java
+++ src/main/java/com/yue/websocket/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.yue.websocket.config;
 
+import com.alibaba.fastjson.support.spring.GenericFastJsonRedisSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
@@ -28,7 +29,7 @@ public class RedisConfig {
 
     @Bean
     public RedisTemplate<Serializable, Object> getRedisTemplate() {
-        GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer();
+        GenericFastJsonRedisSerializer serializer = new GenericFastJsonRedisSerializer();
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setHashKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(serializer);
